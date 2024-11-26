2c193c4d0015beabd00f816e0bffc93d4d7ad25b
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-11-26 02:26:36 -0800

    Initial Commit

---
+++ Activity32/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Activity32
+//
+//  Created by Chatchapong Thuaypha on 11/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
